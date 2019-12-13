596c4c10a409539122f894d369c176ecfe35e4e4
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-13 14:04:47 +0300

    created primary logic around the site, recreted user model to let login using email address

--- auth_p/FacebookGoogleAuth/models.py
+++ auth_p/FacebookGoogleAuth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
--- auth_p/FacebookGoogleAuth/views.py
+++ auth_p/FacebookGoogleAuth/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .forms import *
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Authenticated successfully')
+            else:
+                return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'FacebookGoogleAuth/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save()
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'FacebookGoogleAuth/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, 'FacebookGoogleAuth/register.html', {'user_form': user_form})
+
